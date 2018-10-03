41a877a2e8374e0794da02a7b7b8875cb573e208
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-02 17:52:10 -0700

    initial layout and setup for dicee

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Cody on 10/2/18.
+//  Copyright © 2018 Codyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
