93712f164a6da1bd37709e8855085672532428c0
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-03 09:43:39 -0700

    writing stubs for UI functions and connecting

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1:Int = 0
+    var randomDiceIndex2:Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+    }
+    
 }
 
