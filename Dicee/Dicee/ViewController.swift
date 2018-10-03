ed97f65c6d29f002edba5685719efa6563b414d9
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-03 10:38:17 -0700

    updating graphics on screen with roll button
    dice now reflect random values and display the appropriate image

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1:Int = 0
     var randomDiceIndex2:Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -25,6 +27,11 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        print(randomDiceIndex1)
+        //print(randomDiceIndex2)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
---
+++ variablesConstantsAndDataTypes.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var myAge:Int = 44
+
+myAge = 45
+
+let myName:String = "Cody"
+let myLastName:String = "Tolmasoff"
+
+let myAgeInTenYears:Int = myAge + 10
+
+let myFullName:String = "\(myName) \(myLastName)"
+
+let myDetails:String = "\(myFullName), \(myAge)"
